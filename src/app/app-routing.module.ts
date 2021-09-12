36379b6aa5266826ee4b8e06080dd0bf88121364
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2021-09-12 23:31:06 +0200

    feat: add specifc route for each component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,21 +16,7 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  /*
-  heroXXX: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-  */
-
   heroes: Hero[] = [];
-
-  selectedHero?: Hero;
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id= ${hero.id}`);
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
